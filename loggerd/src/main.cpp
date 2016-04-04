f9fa985bbec00ea884af11281c32fa2ef708b11f
Janus Erasmus <janus@kses.net>
2016-04-04 16:45:59 +0200

    Add Raspberry Pi loggerd' '

---
+++ loggerd/src/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+int main(int argc, char ** argv)
+{
+
+
+	printf("DONE\n");
+
+	return 0;
+}
